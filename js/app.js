1e6684d70475c5403dc62773492cecbb29c6c25a
KTachibanaM <whj19931115@gmail.com>
2017-01-21 13:29:22 +0800

    routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 var app = angular.module('cloudEmoticonWebsite', ['ngRoute']);
+
+app.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $routeProvider
+        .when('/', {
+            templateUrl: './pages/homepage.html'
+        })
+        .when('/mobile', {
+            template: 'mobile'
+        })
+        .when('/desktop', {
+            template: 'desktop'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+});
